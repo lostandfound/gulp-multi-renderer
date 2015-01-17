4b2f12c40c1dceec9c8f4c541f80574f7de3e20e
Richard Fussenegger <richard@fussenegger.info>
2015-01-17 21:05:04 +0100

    Automated formatting by IDE for consistent style

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-'use strict';
+/* jshint node:true */
+"use strict";
 
-var gutil = require('gulp-util');
+var gutil = require("gulp-util");
 var PluginError = gutil.PluginError;
-
-var through = require('through2');
-var fs = require('fs');
-
-var ejs = require('ejs');
-var mustache = require('mustache');
-var jade = require('jade');
-
+var through = require("through2");
+var fs = require("fs");
+var ejs = require("ejs");
+var mustache = require("mustache");
+var jade = require("jade");
 
 function renderTemplate(template, data, engine) {
 
-    engine = engine || 'ejs';
+    engine = engine || "ejs";
 
     // Jade
-    if(engine == 'jade') {
+    if (engine === "jade") {
         try {
             return new Buffer(jade.render(template, data));
         } catch (e) {}
     }
 
     // mustache
-    if(engine == 'mustache') {
+    if (engine === "mustache") {
         try {
             return new Buffer(mustache.render(template, data));
         } catch (e) {}
     }
 
     // EJS
-    if(engine == 'ejs') {
+    if (engine === "ejs") {
         try {
             return new Buffer(ejs.render(template, data));
         } catch (e) {}
@@ -45,25 +43,25 @@ function findTemplate(data, layout, dir) {
 
     // Jadeのレイアウト
     try {
-        template = fs.readFileSync(dir + "/" + layout + ".jade", 'utf-8');
+        template = fs.readFileSync(dir + "/" + layout + ".jade", "utf-8");
         return renderTemplate(template, data, "jade");
     } catch (e) {}
 
     // Mustacheのレイアウト
     try {
-        template = fs.readFileSync(dir + "/" + layout + ".mustache", 'utf-8');
+        template = fs.readFileSync(dir + "/" + layout + ".mustache", "utf-8");
         return renderTemplate(template, data, "mustache");
     } catch (e) {}
 
     // EJSのレイアウト
     try {
-        template = fs.readFileSync(dir + "/" + layout + ".ejs", 'utf-8');
+        template = fs.readFileSync(dir + "/" + layout + ".ejs", "utf-8");
         return renderTemplate(template, data, "ejs");
     } catch (e) {}
 
     // デフォルトレイアウト
     try {
-        template = fs.readFileSync(dir + "/" + "default.ejs", 'utf-8');
+        template = fs.readFileSync(dir + "/" + "default.ejs", "utf-8");
         return renderTemplate(template, data, "ejs");
     } catch (e) {
         console.log(e.toString());
@@ -91,19 +89,19 @@ function gulpMultiRenderer(options) {
         }
 
         // initialize local data
-        var local =  {};
+        var local = {};
         local[options.property] = file[options.property];
         local[options.property].engine = typeof local[options.property].engine === "undefined" ? "ejs" : local[options.property].engine;
 
         // render template within the file.contents
-        if(options.target == 'content') {
+        if (options.target === "content") {
             file.contents = renderTemplate(String(file.contents), local, local[options.property].engine);
             this.push(file);
             return callback();
         }
 
         // render template file to wrap the file.contents
-        if(options.target == 'wrap') {
+        if (options.target === "wrap") {
             var layout = typeof local[options.property].layout === "undefined" ? "default" : local[options.property].layout;
             local.contents = file.contents;
 
@@ -117,6 +115,6 @@ function gulpMultiRenderer(options) {
     });
 
     return output;
-};
+}
 
 module.exports = gulpMultiRenderer;
