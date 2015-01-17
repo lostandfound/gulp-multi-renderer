94692b61aa3d9d821919eac2952c866a6e044b93
Richard Fussenegger <richard@fussenegger.info>
2015-01-17 21:16:42 +0100

    DRY: put it all together

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 /* jshint node:true */
 "use strict";
 
-var gutil = require("gulp-util");
-var PluginError = gutil.PluginError;
-var through = require("through2");
-var fs = require("fs");
-var ejs = require("ejs");
-var mustache = require("mustache");
-var jade = require("jade");
+var defaultFrontMatterOptions = {
+    engine: "ejs",
+    layout: "default"
+};
 
 /**
  * Default options for this module.
@@ -19,16 +16,8 @@ var defaultOptions = {
     templateDir: "./layouts"
 };
 
-function renderTemplate(template, data, engine) {
-    return new Buffer(require(engine).render(template, data));
-}
-
-function findTemplate(data, layout, dir) {
-    return renderTemplate(fs.readFileSync(dir + "/" + layout + "." + data.engine), data, data.engine, "utf-8");
-}
-
 function gulpMultiRenderer(options) {
-    return through.obj(function (file, encoding, callback) {
+    return require("through2").obj(function (file, encoding, callback) {
         // Only try to process the stream if it actually _is_ a stream.
         if (!file.isNull() || !file.isStream()) {
             var merge = require("merge");
@@ -36,26 +25,29 @@ function gulpMultiRenderer(options) {
             // Merge default with user supplied options.
             options = merge(defaultOptions, options);
 
-            // initialize local data
-            var local = {};
-            local[options.property] = file[options.property];
-            local[options.property].engine = typeof local[options.property].engine === "undefined" ? "ejs" : local[options.property].engine;
+            // Merge default front matter with extracted options.
+            file[options.property] = merge(defaultFrontMatterOptions, file[options.property]);
 
-            // render template within the file.contents
-            if (options.target === "content") {
-                file.contents = renderTemplate(String(file.contents), local, local[options.property].engine);
-                this.push(file);
-                return callback();
-            }
+            // Allow direct access of front matter options.
+            var template = file[options.property];
 
-            // render template file to wrap the file.contents
-            if (options.target === "wrap") {
-                var layout = typeof local[options.property].layout === "undefined" ? "default" : local[options.property].layout;
-                local.contents = file.contents;
+            try {
+                // We only want to process the markdown file.
+                if (options.target === "content") {
+                    file.contents = String(file.contents);
+                }
+                // We want to process the actual template with the desired renderer.
+                else if (options.target === "wrap") {
+                    file.contents = require("fs").readFileSync(
+                        options.templateDir + "/" + template.layout + "." + template.engine,
+                        encoding
+                    );
+                }
 
-                file.contents = findTemplate(local, layout, options.templateDir);
-                this.push(file);
-                return callback();
+                // Let the actual rendering engine perform its magic.
+                file.contents = new Buffer(require(template.engine).render(file.contents, file));
+            } catch (e) {
+                require("gulp-util").PluginError("gulp-multi-renderer", e);
             }
         }
 
